ddd069f677a4928847daf47e91db13a4fd41a1a8
Tania <t.ilinykh1@g.nsu.ru>
2024-03-19 19:07:04 +0700

    add ending to the if-structure

--- main.py
+++ main.py
@@ -22,3 +22,17 @@ elif depos_name == '13.5% на 12мес - Тинькофф':
     deposit_sum(begin_sum, percent[1], period[1])
 elif depos_name == '14% на 12мес - Сбер':
     deposit_sum(begin_sum, percent[2], period[2])
+elif depos_name == '14.09% на 6мес - ВТБ':
+    deposit_sum(begin_sum, percent[3], period[3])
+elif depos_name == '14.75% на 36мес - АльфаБанк':
+    deposit_sum(begin_sum, percent[4], period[4])
+elif depos_name == '15% на 3мес- Тинькофф':
+    deposit_sum(begin_sum, percent[5], period[5])
+elif depos_name == '15.25% на 36мес - БанкЛевобережный':
+    deposit_sum(begin_sum, percent[6], period[6])
+elif depos_name == '16% на 6мес - ВТБ':
+    deposit_sum(begin_sum, percent[7], period[7])
+elif depos_name == '16% на 36мес - АльфаБанк':
+    deposit_sum(begin_sum, percent[8], period[8])
+else:
+    deposit_sum(begin_sum, percent[9], period[9])
